f36f1579de49e36eb894944bfcbff8948860889d
Matt Holmberg <mholmberg13@gmail.com>
2021-02-24 01:08:38 -0700

    Finished index.js, initial commit.

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+
+// import api
+const orders = require('./routes/api/orders')
+
+// nitializes the express app
+const app = express()
+
+// set up CORS
+app.use(cors())
+// convert API response to json
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// import db credentials
+const db = require('./config/keys').mongoURI
+
+// initializes db with credentials 
+mongoose.set('useFindAndModify', false)
+mongoose
+    .connect(db, () => {}, {useNewUrlParser: true})
+    .then(() => console.log('Mongo Database Connected'))
+    .catch(err => console.log(err))
+
+
+// route for API interaction
+app.use('/api/orders', orders)
+
+// sets port number
+const port = process.env.PORT || 5000
+
+// initialize server
+server = app.listen(port, () => console.log(`Server running on port ${port}`))
