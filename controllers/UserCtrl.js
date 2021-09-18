71d29e9e477a22ccb5c2a934fdefad66b93c9e0d
Matt Holmberg <mholmberg13@gmail.com>
2021-09-18 17:12:08 -0600

    Logout and Admin routes added.

--- controllers/UserCtrl.js
+++ controllers/UserCtrl.js
@@ -94,20 +94,80 @@ const userCtrl = {
     getAccessToken: (req, res) => {
         try {
             const rf_token = req.cookies.refreshtoken
-            console.log(rf_token)
-            if(!rf_token) return res.status(400).json({msg: 'Please Log In.'})
+            if(!rf_token) return res.status(400).json({msg: 'Please Login.'})
 
             jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-                if(err) return res.status(400).json({msg: 'Please Log In.'})
+                if(err) return res.status(400).json({msg: 'Please Login.'})
             
-                console.log(user)
+                const access_token = createAccessToken({id: user.id})
+                res.json({access_token})
             })
 
 
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
+    },
+    forgotPassword: async (req, res) => {
+        try {
+            const {email} = req.body
+            const user = await Users.findOne({email})
+            if(!user) return res.status(400).json({msg: "This email does not exist."})
+
+            const access_token = createAccessToken({id: user._id})
+            const url = `${CLIENT_URL}/user/reset/${access_token}`
+
+            sendMail(email, url, "Reset your password")
+            res.json({msg: "Re-send the password, please check your email."})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    resetPassword: async (req, res) => {
+        try {
+            const {password} = req.body
+            const passwordHash = await bcrypt.hash(password, 12)
+
+            await Users.findOneAndUpdate({_id: req.user.id}, {
+                password: passwordHash
+            })
+
+            res.json({msg: "Password successfully changed!"})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    getUserInfo: async (req, res) => {
+        try {
+            const user = await Users.findById(req.user.id).select('-password')
+
+
+            res.json(user)
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    getAllUserInfo: async (req, res) => {
+        try {
+            console.log(req.user)
+            const users = await Users.find().select('-password')
+
+            res.json(users)
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    logout: async (req, res) => {
+        try {
+            res.clearCookie('refreshtoken', {path: '/user/refresh_token'})
+            return res.json({msg: "Logged Out."})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
     }
+
 }
 
 function validateEmail(email) {
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const userCtrl = require('../controllers/UserCtrl')
+const auth = require('../middleware/auth')
+const authAdmin = require('../middleware/authAdmin')
 
 router.post('/register', userCtrl.register)
 
@@ -8,5 +10,15 @@ router.post('/activation', userCtrl.activateEmail)
 router.post('/login', userCtrl.login)
 
 router.post('/refresh_token', userCtrl.getAccessToken)
+
+router.post('/forgot', userCtrl.forgotPassword)
+
+router.post('/reset', auth, userCtrl.resetPassword)
+
+router.get('/info', auth, userCtrl.getUserInfo)
+
+router.get('/all_info', auth, authAdmin, userCtrl.getAllUserInfo)
+
+router.get('/logout', auth, authAdmin, userCtrl.logout)
 
 module.exports = router
